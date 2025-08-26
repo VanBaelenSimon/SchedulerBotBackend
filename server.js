bf76deb43c660d4e1e01958fbc551a07c5428891
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2025-08-26 12:39:15 +0200

    Changed " " into ' '

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { db } = require("./config/firebase");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./config/firebase');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/availability", require("./routes/availability"));
-app.use("/teams", require("./routes/teams"));
+app.use('/availability', require('./routes/availability'));
+app.use('/teams', require('./routes/teams'));
 
 // Health check
-app.get("/", (req, res) => {
-  res.send("Availability Backend is running");
+app.get('/', (req, res) => {
+  res.send('Availability Backend is running');
 });
 
 //// Clear team's schedule
-app.get("/teams/clear/:guildId/:userId", async (req, res) => {
-  const API_URL = process.env.API_URL || "http://localhost:3000";
+app.get('/teams/clear/:guildId/:userId', async (req, res) => {
+  const API_URL = process.env.API_URL || 'http://localhost:3000';
   try {
     const { guildId, userId } = req.params;
 
     const teamSnapshot = await db
-      .collection("teams")
-      .where("guildId", "==", guildId)
-      .where("createdBy", "==", userId)
+      .collection('teams')
+      .where('guildId', '==', guildId)
+      .where('createdBy', '==', userId)
       .limit(1)
       .get();
 
     if (teamSnapshot.empty) {
       return res.status(403).json({
         success: false,
-        message: "You are not the creator of any team in this guild.",
+        message: 'You are not the creator of any team in this guild.',
       });
     }
     const team = {
@@ -42,16 +42,16 @@ app.get("/teams/clear/:guildId/:userId", async (req, res) => {
     const teamMembers = team.members;
     for (const memberId of teamMembers) {
       const availabilitySnapshot = await db
-        .collection("availabilities")
-        .where("guildId", "==", guildId)
-        .where("userId", "==", memberId)
+        .collection('availabilities')
+        .where('guildId', '==', guildId)
+        .where('userId', '==', memberId)
         .get();
 
       if (availabilitySnapshot.empty) {
         return res.status(403).json({
           success: false,
           message:
-            "Team members do not have any availabilties set within this guild.",
+            'Team members do not have any availabilties set within this guild.',
         });
       }
       const availabilities = {
@@ -62,7 +62,7 @@ app.get("/teams/clear/:guildId/:userId", async (req, res) => {
       const res = await fetch(
         `${API_URL}/availability/${guildId}/${availabilities.userId}/${availabilities.shortId}`,
         {
-          method: "DELETE",
+          method: 'DELETE',
         }
       );
     }
@@ -73,7 +73,7 @@ app.get("/teams/clear/:guildId/:userId", async (req, res) => {
     console.log(error);
     res
       .status(500)
-      .json({ success: false, message: "Failed to clear schedule command" });
+      .json({ success: false, message: 'Failed to clear schedule command' });
   }
 });
 
