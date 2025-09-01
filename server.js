56e07e219f193f87b80045e1ff76b4d617c097bf
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2025-09-01 14:46:03 +0200

    Added allow CORS and use of cookie-parcer

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+// Middleware
+app.use(cookieParser())
 
 // Routes
 app.use('/availability', require('./routes/availability'));
